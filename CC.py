e4771a0673ddd7a09cf4688bcfb3ccf40b0bf793
Syed Khaild Tipu Razvi <110707546+SKTR13055@users.noreply.github.com>
2024-07-25 19:49:31 +0530

    Add files via upload

--- CC.py
+++ CC.py
@@ -1,18 +1,6 @@
-while True:
-    
-    print("Welcome to the Caesar Cipher\nPlease Select to either encrypt or decrypt the data")
-    print("Press '1' for Encryption")
-    print("Press '2' for Decryption")
-    print("Press '3' to Quit")
-
-    option = int(input("Choose your option\n"))
-
-    if(option == 1):
-     print("You've chosen Encryption")
-     plaintxt = input("Enter plain text to encrypt\n")
-     shift = int(input("Enter the shift number for encryption\n"))
-     pt = ""
-     for char in plaintxt:
+def encrypt(plaintxt,shift):
+    pt = ""
+    for char in plaintxt:
             if(char.isupper()):
                n = ord(char) - ord('A')
                newp = (n + shift) % 26 
@@ -25,14 +13,10 @@ while True:
                pt += chr(newc)
             else: 
                 pt += char
-     print(pt)
-                
-    elif(option == 2):
-     print("You've chosen Decryption")
-     ciphertxt = (input("Enter Cipher text to decrypt\n"))
-     shift = int(input("Enter the shift number for decryption\n"))
-     ct = ""
-     for char in ciphertxt:
+    return pt
+def decrypt(ciphertxt,shift):
+    ct = ""
+    for char in ciphertxt:
         if(char.isupper()):
             n = ord(char) - ord("A")
             newp = (n - shift) % 26
@@ -45,11 +29,28 @@ while True:
             ct += chr(newc)
         else:
             ct += char
-     print(ct)
-    
-    elif(option == 3):
-     print("Goodbye! :D")
-     break
-    else:
-     print("Invalid Input")  
+    return ct
+print("Welcome to the Caesar Cipher\nPlease Select to either encrypt or decrypt the data\n")
+while True:
+ print("Press '1' for Encryption")
+ print("Press '2' for Decryption")
+ print("Press '3' to Quit")
+ choice = int(input("Enter your choice: "))
  
+ if (choice == 1):
+     plaintxt = input("Enter a plain text to encrypt\n")
+     shift = int(input("Enter the shift number\n"))
+     encrypted_text = encrypt(plaintxt,shift)
+     print(f"Encrypted Text: {encrypted_text}")
+
+ elif(choice == 2):
+    ciphertxt = input("Enter the cipher text to decrypt\n")
+    shift = int(input("Enter the shift number\n"))
+    decrypted_text = decrypt(ciphertxt,shift)
+    print(f"Decrypted Text: {decrypted_text}")
+ 
+ elif(choice == 3):
+     print("Exiting the Program, Goodbye :D")
+     break
+ else:
+     print("Invalid Choice: Please select 1,2, or 3")
